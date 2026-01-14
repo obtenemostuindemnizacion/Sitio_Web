e27abfa6231f6647c7cc9a34c56d8301af522197
obtenemostuindemnizacion <obtenemostuindemnizacion@gmail.com>
2026-01-14 19:31:40 +0100

    Update vite.config.ts

--- obtenemos-tu-indemnizacion/vite.config.ts
+++ obtenemos-tu-indemnizacion/vite.config.ts
@@ -1,11 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Esto permite que el código use process.env.API_KEY en Vercel
     'process.env': process.env
   }
 });
